147acd1c4fb67a4ba35a8144be3d02e945326d3d
grannyMoon <arve@arve.no>
2014-07-25 03:38:16 +0200

    First ci

---
+++ 404.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package WordPress
+ * @subpackage Sverresborg Idrettsforening
+ * @since Sverresborg Idrettsforening 1.0
+ */
+ get_header(); ?>
+
+	<h2><?php _e('Error 404 - Page Not Found','sif'); ?></h2>
+
+<?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
---
+++ functions-menu.php
@@ -0,0 +1,11 @@
+<?php
+
+/* 
+ * Site wide custom menu
+ * July 2014 Arve Gulbrandsen arve@arve.no
+ */
+
+function get_sitewide_menu() {
+  print_r(wp_get_sites());
+  print "her";
+}
