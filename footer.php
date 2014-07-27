9e497b5ad4f1f0ec711de1e6f07376db929db4b7
grannyMoon <arve@arve.no>
2014-07-27 04:44:35 +0200

    1

--- footer.php
+++ footer.php
@@ -21,9 +21,6 @@
 
 <!-- jQuery is called via the WordPress-friendly way via functions.php -->
 
-<!-- this is where we put our custom functions: This is in the enqueue function currently -->
-<script src="<?php bloginfo('template_directory'); ?>/static/js/footer.js"></script> 
-
 <!-- Asynchronous google analytics; this is the official snippet.
          Replace UA-XXXXXX-XX with your site's ID and domainname.com with your domain, then uncomment to enable.
 
