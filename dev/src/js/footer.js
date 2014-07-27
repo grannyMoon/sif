03d7670ffe7ff0a0307fd01180ba3792d336ce82
grannyMoon <arve@arve.no>
2014-07-27 04:44:07 +0200

    1

--- dev/src/js/footer.js
+++ dev/src/js/footer.js
@@ -1,1 +1,13 @@
 /* This js is after anything else on the page */
+
+(function($) {
+  
+  $( document ).ready(function() {
+    $("#sif-menu >> a").prop("href", "#");
+    $(".sif-submenu").hide();
+    $("#sif-menu >> a").click(function(e) {
+      console.log($(this).next().toggle('fast'));
+    });
+  });
+  
+})(jQuery);
